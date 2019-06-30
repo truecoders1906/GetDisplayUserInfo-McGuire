3e599040962a7a1923c8388bf7ae8253dbf1100a
McGuire Price <mcguire02170@gmail.com>
2019-06-29 22:24:54 -0500

    Basic Layout

---
+++ GetDisplayUserInfo-McGuire/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GetDisplayUserInfo_McGuire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey there! I just need some basic info!");
+
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many pets do you have?");
+            int numberOfPets = int.Parse(Console.ReadLine());
+
+           //Do a list or array of pet names
+
+        }
+    }
+}
